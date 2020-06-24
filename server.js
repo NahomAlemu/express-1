4d6edf236965c87f866f6b1fe7e703e8c7e23c1f
NahomAlemu <55855783+NahomAlemu@users.noreply.github.com>
2020-06-23 21:40:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
-var express  = require('express');
+var express = require('express');
 var data = require('./data/test.json');
-
 var app = express();
 const PORT = 3000;
 
-//we can now serve up static pages
-app.use(express.static(__dirname));
+
+app.set('view engine', 'ejs'); //sent views to EJS
+
+//this is how a rout is built in express
+app.get('/',(req, res)=>{
+  var title = "Our Home Page";
+  res.render('pages/index',{title:title});
+});
+
+app.get('/users',(req, res)=>{
+  var title = "Our Home Page";
+  res.render('users/index',{
+    title:title,
+    users:data
+  });
+});
 
 //app.listen(3000);
-
 app.listen(PORT,()=>{
   console.log(`App is running on port ${PORT}`);
-  console.log(data);
+  //console.log(data);
 });
