60784cea8149cc95d57ce55255a3a815b7fda552
NahomAlemu <55855783+NahomAlemu@users.noreply.github.com>
2020-06-23 18:09:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 var express = require('express');
+var data = require('./data/test.json');
+
 var app = express();
-app.listen(3000);
+const PORT = 3000;
+
+app.use(express.statics(__dirname));
+
+//app.listen(300);
+
+app.listen(PORT,()=>{
+  console.log(`App is running on port ${PORT}`);
+
+});
+
